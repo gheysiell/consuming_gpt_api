8816bfbd68a42d4c422ad04f75ca7862fa748578
gheysiell <gheysiellfarukzinn@hotmail.com>
2023-04-23 22:17:21 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# consuming_gpt_api
+
+<div> 
+    <img src="https://raw.githubusercontent.com/gheysiell/images/master/consuming_gpt_api.png" alt="consuming_gpt_api demonstration">
+</div>
+<div> <h2> Web interface with one chat to ask to the chat GPT and her response, consuming chat GPT api to this. </h2> </div>
+<div> <h5> ° Open the index.html file in your browser </h5> </div>
+<div> <h5> ° Crate account in open ai and get the api key in url: https://platform.openai.com/account/api-keys </h5> </div>
+<div> <h5> ° Get your api key and paste in script.js in const OPENAI_API_KEY </h5> </div>
---
+++ script.js
@@ -0,0 +1,57 @@
+const inputQuestion = document.getElementById("inputQuestion")
+const result = document.getElementById("result")
+
+inputQuestion.addEventListener("keypress", e => {
+    if (inputQuestion.value && e.key == "Enter") {
+        sendQuestion()
+    }
+})
+
+const OPENAI_API_KEY = "your_api_key_here"
+
+function sendQuestion() {
+    var sQuestion = inputQuestion.value
+
+    fetch("https://api.openai.com/v1/completions", {
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${OPENAI_API_KEY}`,
+        },
+        body: JSON.stringify({
+            model: "text-davinci-003",
+            prompt: sQuestion,
+            max_tokens: 2048,
+            temperature: 0.5,
+        })
+    })
+    .then(res => res.json())
+    .then(json => {
+        if (result.vaule) result.value += "\n"
+
+        if (json.error?.message) {
+            result.value += `Error: ${json.error.message}`
+        } else if (json.choices?.[0].text) {
+            var text = json.choices[0].text || "Sem resposta"
+
+            result.value += "Chat GPT: " + text
+        }
+
+        result.scrollTop = result.scrollHeight
+    })
+    .catch(err => console.log(`Error: ${err}`))
+    .finally(() => {
+        inputQuestion.value = ""
+        inputQuestion.disabled = false
+        inputQuestion.focus()
+    })
+
+    if (result.value) result.value += "\n\n\n"
+
+    result.value += `Eu: ${sQuestion}\n`
+    inputQuestion.value = "Carregando..."
+    inputQuestion.disabled = true
+
+    result.scrollTop = result.scrollHeight
+}
